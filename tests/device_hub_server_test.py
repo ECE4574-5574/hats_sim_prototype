60cb5d980d357667b04bfe986018428fb2650996
Mark Koninckx <koninckx@vt.edu>
2015-03-02 15:14:45 -0500

    Update unit tests to test the new get_device method on the device hub server. Assign random port numbers at setup because unittest runs multiple tests in parallel.

--- tests/device_hub_server_test.py
+++ tests/device_hub_server_test.py
@@ -10,7 +10,8 @@ from hats_sim import simulated_devices as sd
 
 class DeviceHubServerTest(unittest.TestCase):
 	def setUp(self):
-		self.server = dhs.DeviceHubRequestServer(('',8080), dhs.DeviceHubRequestHandler)
+		self.server = dhs.DeviceHubRequestServer(('',0), dhs.DeviceHubRequestHandler)
+		self.port = self.server.socket.getsockname()[1]
 		self.atriumlight = sd.LightSwitch('/user/home/devices/atrium/lightswitch', False)
 		self.kitchenlight = sd.LightSwitch('/user/home/devices/kitchen/lightswitch', False)
 		self.server.add_device(self.atriumlight)
@@ -18,17 +19,22 @@ class DeviceHubServerTest(unittest.TestCase):
 		self.server.logger.setLevel(logging.DEBUG)
 		self.thread = dhs.serveInBackground(self.server)
 
+	def testGetDevice(self):
+		self.assertNotEqual(self.server.get_device('/user/home/devices/atrium/lightswitch'), None)
+		self.assertEqual(self.server.get_device('/some/path/that/is/nonsense'), None)
+
 	def testGET(self):
-		conn = httplib.HTTPConnection('localhost:8080')
+		conn = httplib.HTTPConnection('localhost', self.port)
 		conn.request('GET', '/user/home/devices/atrium/lightswitch')
 		self.assertEqual(conn.getresponse().status, 200)
 		conn.request('GET', '/some/path/that/is/nonsense')
-		self.assertEqual(conn.getresponse().status, 400)
+		self.assertEqual(conn.getresponse().status, 404)
 
 	def tearDown(self):
-		conn = httplib.HTTPConnection('localhost:8080')
+		conn = httplib.HTTPConnection('localhost', self.port)
 		conn.request('QUIT', '')
 		self.thread.join(30)
+		
 
 if __name__ == '__main__':
 	unittest.main()
