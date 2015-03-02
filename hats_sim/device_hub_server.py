3996898e7cd4569f5d8435f0b59f522e6eefcc7f
Mark Koninckx <koninckx@vt.edu>
2015-03-01 20:58:35 -0500

    Allow device hub simulation to store a list of devices and respond to GET requests based on whether that device appears in the list.

--- hats_sim/device_hub_server.py
+++ hats_sim/device_hub_server.py
@@ -2,15 +2,18 @@ from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import logging
 import threading
 import time
+from simulated_devices import HatsSimDevice, LightSwitch, Thermostat
 
 LISTEN_PORT = 8080
 
 class DeviceHubRequestHandler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
-		self.send_response(200)
+		if self.path in self.server.devices:
+			self.send_response(200)
+		else:
+			self.send_response(404)
 		self.end_headers()
-		self.server.logger.debug('I\'m pretending to handle a GET...')
 		return
 	
 	def do_POST(self):
@@ -29,6 +32,7 @@ class DeviceHubRequestServer(HTTPServer):
 
 	logger = None
 	shouldStop = False
+	devices = {}
 	
 	def __init__ (self, server_address, RequestHandlerClass):
 		HTTPServer.__init__(self, server_address, RequestHandlerClass)
@@ -38,18 +42,25 @@ class DeviceHubRequestServer(HTTPServer):
 		while not self.shouldStop:
 			self.handle_request()
 
-def runServer():
-	logging.basicConfig(format='%(asctime)s %(message)s')
-	server=DeviceHubRequestServer(('',LISTEN_PORT), DeviceHubRequestHandler)
-	server.logger.setLevel(logging.DEBUG)
+	def add_device (self, device):
+		self.devices[device.deviceID] = device
+
+def runServer(server):
 	server.serve_forever()
 
-def serveInBackground():
-	thread = threading.Thread(target=runServer)
+def serveInBackground(server):
+	thread = threading.Thread(target=runServer, args=(server,))
 	thread.start()
 
 if __name__ == "__main__":
-	serveInBackground()
+	logging.basicConfig(format='%(asctime)s %(message)s')
+	server=DeviceHubRequestServer(('',LISTEN_PORT), DeviceHubRequestHandler)
+	atriumlight = LightSwitch('/user/home/devices/atrium/lightswitch', False)
+	kitchenlight = LightSwitch('/user/home/devices/kitchen/lightswitch', False)
+	server.add_device(atriumlight)
+	server.add_device(kitchenlight)
+	server.logger.setLevel(logging.DEBUG)
+	serveInBackground(server)
 	try:
 		while True:
 			print 'Serving...'
