3c1a0cdc0198bd0e7021b4aa8ed3e769fd2ea165
Jason Ziglar <jpz@vt.edu>
2015-03-02 17:19:32 -0500

    Making spacing in room.py consistent.

--- hats_sim/room.py
+++ hats_sim/room.py
@@ -1,31 +1,31 @@
 #Room object, contains list of users and devices
 class Room:
-    """Room object"""
-    def __init__(self):
-        self.users = []
-        self.devices = []
+  """Room object"""
+  def __init__(self):
+    self.users = []
+    self.devices = []
 
-    """Setters"""
-    def addUser(self,userID):
-        self.users.append(userID)
-    def addDevice(self,deviceID):
-        self.devices.append(deviceID)
+  """Setters"""
+  def addUser(self,userID):
+    self.users.append(userID)
+  def addDevice(self,deviceID):
+    self.devices.append(deviceID)
 
-    """Getters"""
-    def getUsers(self):
-        return self.users
-    def getDevices(self):
-        return self.devices
+  """Getters"""
+  def getUsers(self):
+    return self.users
+  def getDevices(self):
+    return self.devices
 
-    """Visiting Method"""
-    def visit(self, graph, time):
-        for user in self.users:
-            user.vist(graph,time)
-        for device in self.devices:
-            device.visit(graph,time)
+  """Visiting Method"""
+  def visit(self, graph, time):
+    for user in self.users:
+      user.vist(graph,time)
+    for device in self.devices:
+      device.visit(graph,time)
 
 class Door:
   def __init__(self):
-    pass
+  pass
   def visit(self, graph, time):
-    pass
+  pass
