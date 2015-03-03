2e0471e7027fec0f2b5ee3ac97ab5294ed652aa8
Mark Koninckx <koninckx@vt.edu>
2015-03-02 21:03:54 -0500

    Add toJson to classes in device.py. Correct type and syntax errors.

--- hats_sim/device.py
+++ hats_sim/device.py
@@ -1,13 +1,15 @@
 #Prerana Rane 3/2/2015
 
-class Devices:
+import json
+
+class Device(object):
   def __init__(self):
     self.id = 0
     
 #Light Module    
 class Lights(Device):
   def __init__(self, config):
-    super(Lights, self).__init__(config)
+    super(Device, self).__init__()
     self.status = config.get('status', True)
     self.brightness = config.get('brightness', 1.0)
   def getBrightness(self):
@@ -18,11 +20,14 @@ class Lights(Device):
     self.brightness = 1.0
   def turnOff(self):
     self.brightness = 0.0
- 
+  def toJson(self):
+    return json.dumps({'status' : self.status, 
+                       'brightness' : self.brightness})
+
 #Alarm Module
 class Alarm(Device):
   def __init__(self, config):
-    super(Alarm, self).__init__(config)
+    super(Device, self).__init__()
     self.alarm_status = config.get('alarm_status', True)
     self.smoke_alarm_status = config.get('smoke_alarm_status', True)
   def getStatus(self):
@@ -37,11 +42,14 @@ class Alarm(Device):
     self.smoke_alarm_status = True
   def disableSmokeAlarm(self):
     self.smoke_alarm_status = False
+  def toJson(self):
+    return json.dumps({'alarm_status' : self.alarm_status, 
+                       'smoke_alarm_status' : self.smoke_alarm_status})
 
 #HVAC Module
 class HVAC(Device):
- def __init__(self, config):
-    super(HVAC, self).__init__(config)
+  def __init__(self, config):
+    super(Device, self).__init__()
     self.tempCurrent = config.get('tempCurrent', 85 )
     self.humidityCurrent = config.get('humidityCurrent', 100)
     self.tempDesired = config.get('tempDesired', 80 )
@@ -65,35 +73,47 @@ class HVAC(Device):
       humidity = getHumidity()
       if humidity != self.humidityDesired:
         setHumidity()
+  def toJson(self):
+    return json.dumps({'tempCurrent' : self.tempCurrent,
+                       'humidityCurrent' : self.humidityCurrent,
+                       'tempDesired' : self.tempDesired,
+                       'humidityDesired' : self.humidityDesired,
+                       'isRunning' : self.isRunning})
 
 #Door Module
 class doorLocks(Device):
   def __init__(self, config):
-    super(doorLocks, self).__init__(config)
+    super(Device, self).__init__()
     self.isLocked = config.get('isLocked', True )
     
-    def getLockedStatus(self):
-    	return self.isLocked
-    def toggleDoorLocks(self):
-    	self.isLocked = False
+  def getLockedStatus(self):
+    return self.isLocked
+  def toggleDoorLocks(self):
+    self.isLocked = False
+  def toJson(self):
+    return json.dumps({'isLocked' : self.isLocked})
 
 #Window Module
 class Windows(Device):
   def __init__(self, config):
-    super(Windows, self).__init__(config)
+    super(Device, self).__init__()
     self.isLocked = config.get('isLocked', True )
     self.height = config.get('height', 2 )
     self.isOpen = config.get('isOpen', True )
     
-    def getLockedStatus(self):
-    	return self.isLocked
-    def toggleWindowLocks(self):
-    	self.isLocked = False		
-    def getHeight(self);
-    	return self.height
-    def Open(self):
-    	self.isOpen = True
-    def Close(self):
-    	self.isOpen = False  
-    def setHeight(self):
-    	self.height = 1
+  def getLockedStatus(self):
+    return self.isLocked
+  def toggleWindowLocks(self):
+    self.isLocked = False		
+  def getHeight(self):
+    return self.height
+  def Open(self):
+    self.isOpen = True
+  def Close(self):
+    self.isOpen = False  
+  def setHeight(self):
+    self.height = 1
+  def toJson(self):
+    return json.dumps({'isLocked' : self.isLocked,
+                       'height' : self.height,
+                       'isOpen' : self.isOpen})
