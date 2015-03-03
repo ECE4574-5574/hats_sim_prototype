de6ea5da730d9a7846b792f5e8d10de1947963d7
sscho91 <sscho91@vt.edu>
2015-03-02 22:31:09 -0500

    Update user.py

--- hats_sim/user.py
+++ hats_sim/user.py
@@ -13,3 +13,42 @@ class User:
     # Attempt to move this user to another room
     success = graph.move_user(self.id, a, b)
     # Do stuff here
+    
+    	#checks state of random device within room
+	def stateChecker():
+		# randomly select device from room based on list
+		randdev = random.randint(0,len(get_device()); #from room class
+		
+		currentDev = get_device(randdev);
+		devState = currentDev.status;
+		devTime = currentDev.time;
+		
+		if devState == 'true':
+			#check device time and if users are in room
+			if not getUsers(); #if users list empty in room
+				#check device idle time
+				if devTime > 360: # seconds to 60 minutes
+					currentDev = 'false'
+		elif devState == 'sleep':#if there is a mode for continuous run (stay on)
+			#continue running current settings regardless of user/idle time
+			
+		else:
+
+	def moveChecker(); #check if user can move rooms and check statuses
+		finding = true;
+		randuser = random.randint(0,len(graph.node[node]['data']);  # get random room from list of rooms
+		currentRoom = graph.node[randuser]['data']
+		success = true
+		while success:
+			if currentRoom.door == false:
+				success = false;	# ends search
+				print "Found open room."
+			else:
+				print "Room ", currentRoom, " ", randuser, " is locked.  Continuing search..."
+				randuser += 1;
+				if randuser > len(graph.node[node]['data']:
+					jumpRoom = graph.node[0]['data']; # start beginning of rooms when it comes to end of list
+					success = graph.move_user(self.id, currentRoom, jumpRoom);
+				else:
+					success = graph.move_user(self.id, currentRoom, graph.node[randuser]['data']);
+				
